44e2102d10797630b34ae26dfc001797393c1a4b
Abelmf98 <91120898+Abelmf98@users.noreply.github.com>
2022-04-23 15:01:29 +0200

    alta realizado, falta comprobar el NULL

--- alta.php
+++ alta.php
@@ -9,12 +9,36 @@
 </head>
 <body>
     <?php
+    /*Necesitaremos el archivo de metodos*/
+    require_once 'metodos.php';
+    /*Creamos el objeto de la clase metodos */
+    $objMetodo = new metodos(); 
+    if(!isset($_POST["Enviar"])){
+
+        echo '<h1>Formulario de alta Minijuegos:</h1>';
         echo '<form action="#" method="POST">';
             echo '<input type="text" name="nombre" placeholder="Nombre del Minijuego">';
             echo '<input type="text" name="icono" placeholder="Icono">';
             echo '<input type="text" name="ruta" placeholder="Ruta del archivo">';
             echo '<input type="submit" name="Enviar">';
-        echo '</form>';    
+        echo '</form>';
+    }else{
+        /*Comprobamos si hay campos vacios */
+        if(!empty($_POST["nombre"] && $_POST["icono"] && $_POST["ruta"])){ /*Me falta comprobar el campo que se muestre a NULL en la BBDD */
+        /*Hacemos la consulta */
+        $consultasql = "INSERT INTO miniJuego (nombre, icono, ruta) 
+        VALUES ('".$_POST["nombre"]."','".$_POST["icono"]."','".$_POST["ruta"]."');";
+        $objMetodo->hacerconsulta($consultasql);
+            /*Comprobamos las filas afectadas para añadir los minijuegos */
+            if($objMetodo->comprobarafectada()>0){
+                echo '<p>Se ha añadido satisfactioriamente el minijuego: </p>'.'<p id="nombre">'.$_POST["nombre"].'</p></br>';
+            }else{
+                echo '<p>Se ha producido un error</p>';
+            }
+        }else{
+            echo '<p>Los campos nombre y ruta son obligatorios</p>';
+        }
+    }        
     ?>
 </body>
 </html>
--- conexionbd.php
+++ conexionbd.php
@@ -2,5 +2,5 @@
     //Datos que pasaremos como parametros al contructor de la clase mysqli
     define('servidor', 'localhost');
     define('usuario', '2daw023T');
-    define('password', '');
+    define('password', '1234');
     define('basededatos', 'miniJuegos');
