3794b9e99e4553703e2c19bfe51f3bb8b3718166
Abelmf98 <91120898+Abelmf98@users.noreply.github.com>
2022-04-29 16:51:43 +0200

    borrado hecho

--- borrado.php
+++ borrado.php
@@ -16,17 +16,35 @@
         if(!isset($_POST["Enviar"])){
             
                 echo '<form action="#" method="POST">';
-                    echo '<label for="borrar">¿Estás seguro que quieres borrar este minijuego?</label>';
-                    echo '<input type="">';
-                    echo '<input type="radio" name="borrar" value="1">Si';
-                    echo '<input type="radio" name="borrar" value="0">No';
+                    echo '<label for="borrar">¿Estás seguro que quieres borrar este minijuego?</label><br>';
+                    /*Enviamos la id del minijuego por url para identificarla */
+                    echo '<input type="hidden" name="idMinijuego" value="'.$_GET['i'].'"><br>';
+                    /*Creamos un input radio para preguntarle al usuario si está seguro de querer borrar el campo */
+                    echo 'Si<input type="radio" name="borrar" value="1">';
+                    echo 'No<input type="radio" name="borrar" value="0"><br>';
                     echo '<input type="submit" name="Enviar">';
                 echo '</form>';
         }else{
-
+            /*
+                *Le decimos que si marca Si, el cual tiene value 1, entonces se realizará la consulta DELETE, en este
+                *caso el dato proviene de la tabla miniJuego donde borrará el id que corresponda al minijuego seleccionado
+                *previamente en el listado de los mismos.
+            */
+            if($_POST["borrar"]==1){
+                $consultasql = "DELETE
+                                FROM miniJuego
+                                WHERE idMinijuego=".$_POST['idMinijuego'].";";
+                /*LLamamos al metodo para hacer la consulta y completar la peticion */
+                $objMetodo->hacerconsulta($consultasql);
+            }
+            /*
+                *Creo un header en el cual, cuando terminemos de realizar nuestra peticion en el borrado, nos redirija directamente al
+                *listado.php, en este caso.
+            */
+            header('Location: listado.php');
         }
 
-        header('Location: listado.php');
+        
     ?>
 </body>
 </html>
