397477ad072216842d63ff80ffe402120aed2be1
Abelmf98 <91120898+Abelmf98@users.noreply.github.com>
2022-05-04 20:22:58 +0200

    conexion

--- conexionbd.php
+++ conexionbd.php
@@ -2,5 +2,5 @@
     //Datos que pasaremos como parametros al contructor de la clase mysqli
     define('servidor', 'localhost');
     define('usuario', '2daw023T');
-    define('pasword', '1234');
+    define('password', '1234');
     define('basededatos', 'miniJuegos');
