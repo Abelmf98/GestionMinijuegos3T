eb6a24462015e5a131102f987a1f8314373c6795
Abelmf98 <91120898+Abelmf98@users.noreply.github.com>
2022-05-12 10:09:34 +0200

    Delete v2 directory

--- v2/conexionbd.php
+++
@@ -1,6 +0,0 @@
-<?php
-    //Datos que pasaremos como parametros al contructor de la clase mysqli
-    define('servidor', 'localhost');
-    define('usuario', '2daw023T');
-    define('password', '1234');
-    define('basededatos', 'miniJuegos');
--- v2/script/permisobd.sql
+++
@@ -1,9 +0,0 @@
-/*Abel Mansilla Felipe*/
-
--- Creamos usuario
-
-CREATE USER '2daw023T'@'localhost' IDENTIFIED BY 'password';
-
--- Le otorgamos permisos de seleccionar, añadir, modificar y eliminar sobre la base de datos minijuegos
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON `minijuegos`.* TO '2daw023T'@'localhost';
